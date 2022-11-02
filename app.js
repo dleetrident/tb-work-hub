25c3fd9569ec9449f149d2730e123f1da2aca8bf
Daniel Lee <dleetrident@hotmail.com>
2022-11-02 12:01:27 +0000

    test deploy commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+
+const path = require("path");
+
+const PORT = process.env.PORT || 5000;
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("build"));
+  app.get("*", (req, res) => {
+    req.sendFile(path.resolve(__dirname, "build", "index.html"));
+  });
+}
+
+app.listen(PORT, (err) => {
+  if (err) {
+    return console.log(err);
+  }
+  console.log(`listening on port ${PORT}`);
+});
