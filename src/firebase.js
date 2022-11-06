c829fc5cf5b4e0a5d38640359b8fa025d8a53449
Daniel Lee <dleetrident@hotmail.com>
2022-11-06 16:33:17 +0000

    final

--- src/firebase.js
+++ src/firebase.js
@@ -8,13 +8,14 @@ import "firebase/compat/auth";
 
 // Your web app's Firebase configuration
 const app = firebase.initializeApp({
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  apiKey: "AIzaSyDUYSZicVoMeiUaAcqYo6-qDuxoU6i3BfI",
+  authDomain: "imageupload-28cb9.firebaseapp.com",
+  databaseURL:
+    "https://imageupload-28cb9-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "imageupload-28cb9",
+  storageBucket: "imageupload-28cb9.appspot.com",
+  messagingSenderId: "606803029623",
+  appId: "1:606803029623:web:687bd7cb35c70755ffa072",
 });
 
 // Initialize Firebase
